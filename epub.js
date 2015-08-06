f8cef4581ca915553d4a9e0b7a5ee3bd4ee0a503
andywong418 <wong22@hotmail.co.uk>
2015-08-06 11:07:06 +0100

    First commit

--- epub.js
+++ epub.js
@@ -1,36 +1,150 @@
-var fs = require('fs');
 
+//http://localhost/epub_project/book/9780198501008/OEBPS/xhtml/Chapter1.xhtml
+//http://localhost/~androswong/epub_project/book/9780198501008/OEBPS/xhtml/Chapter1.xhtml
+function showNodeFromCFI(epubcfi, isbn) {
+	var iframe = document.createElement('iframe');
+    getMeta(isbn, function(meta) {
+        getOpf(meta, isbn, function(opf) {
+            var href = getHref(opf, epubcfi);
+            
+            iframe.onload = function() {
+                var epubDoc = iframe.contentDocument;
+                var node = getNode(epubDoc, epubcfi);
+                console.log(node);
+                node.scrollIntoView();
+                
+                
+                
+            };
+             iframe.src = "../../../~androswong/epub_project/book/" + isbn + "/OEBPS/" + href;
+             iframe.width = "1000";
+             iframe.height = "500";
+             
+             
+        });
+    });
+    $(iframe).appendTo("body");
+}
 
-function epub_read(epub_cfi) {
- try{
-     
+// get the xml of the container.xml file in META-INF dir, and feed to callback
+function getMeta(isbn, callback) {
+    $.get('../../../~androswong/epub_project/book/' + isbn + '/META-INF/container.xml', function(meta) {
+        callback(meta);
+    }, 'xml');
 
- 	 // Get the EPUB's content document
+}
 
- 	 var packageDocument = get_the_package_document();
+// get the xml of the content.opf file and feed to callback
+function getOpf(meta, xml_isbn, callback) {
+    var href = meta.getElementsByTagName('rootfile')[0].getAttribute('full-path');
+    $.get('../../../~androswong/epub_project/book/' + xml_isbn + '/' + href, function(opf) {
+        callback(opf);
+    }, 'xml');
+}
 
- 	  // Get a reference to the "top level" content document in the CFI
-    hrefOfContentDoc = EPUBcfi.Interpreter.getContentDocHref(epub_cfi, packageDocument);
+// get the href of the html file from the opfXML
+//CONSOLE.LOG EVERYTHING when you debug
+function getHref(opf, epubcfi) {
+    var href;
 
-    // Load the content document
-    var contentDocument = get_the_content_document();
+    var idRef;
+    // TODO
+    var parse= epubcfi
+    .split('epubcfi(')[1] //removes epub
+    .split(')')[0] //removes ) bracket
+    .split('/'); //splits into array
 
-   // Open up the location in an iframe the location specified by the cfi
-    var target = EPUBcfi.Interpreter.getTargetElement(epub_cfi, contentDocument);
-
-    //inject iframe into location specified by cfi. so when the file is loaded  an iframe is opened pointing to that specific location.
-
-    return EPUBcfi.Interpreter.injectRangeElements(epub_cfi, contentDocument, "<iframe></iframe");
-
+   
+   
+    
+    var xmlHref = parse[2]; //gets the 4[chapter01]!
+   
+    var firstSquarePosition = xmlHref.indexOf('[');//gets position of [
+    var lastSquarePosition = xmlHref.indexOf(']');//gets position of ]
+    //assuming there is id referral
+    var integerString = ""; //initalise string
+       //get the id inside the [] brackets
+    for (var i = firstSquarePosition + 1; i <lastSquarePosition; i++) {
+             integerString += xmlHref[i];
+    }
+    
+    idRef = opf.querySelector("#" + integerString); //gets the item id and references the element
+    href = idRef.getAttribute("href"); //gets the href attribute of said element
+        
+  
+    
 
 
     
+    return href;
+}
+
+
+// get the node specified by the epub-cfi in the given document
+//Check if the number is divisble by two.
+//make it recur if it's an odd number
+
+
+function getNode(contentDocument, epubcfi) {
+
+    var parse= epubcfi
+    .split('epubcfi(')[1] //removes epub
+    .split(')')[0] //removes ) bracket
+    .split('/'); //splits into array
+
+    var cfiArray = parse.splice(3,3); //removing " ", "/6" and "/4" etc
     
 
-    })
- }
+    var nodeTracer = contentDocument.childNodes[0];
 
- catch(err){
- 	throw err;
- }
+    
+   
+    for( var counter = 0; counter < cfiArray.length; counter++){
+       
+       
+             var initSquarePosition = cfiArray[counter].indexOf('[')//get pos of [
+             var finalSquarePosition = cfiArray[counter].indexOf(']'); //get pos of ]
+             //if there's no id just use the number
+             if (initSquarePosition === -1 ) {
+                 if (cfiArray[counter] % 2 == 0) {
+                  var stringConverter = parseInt(cfiArray[counter]);
+                  
+                  console.log(stringConverter);
+                  nodeTracer = nodeTracer.childNodes[stringConverter -1];
+
+                 }
+                 else {
+                    return null;
+                 }
+
+
+             }
+               //use the id
+             else {
+                      
+                    var idString = ""; //initalise string
+                  //get the id inside the [] brackets
+                  
+                    for (var j = initSquarePosition + 1; j < finalSquarePosition; j++) {
+                         idString += cfiArray[counter][j];
+                    }
+                    
+                    if(contentDocument.getElementById(idString) !== null){
+                        nodeTracer = contentDocument.getElementById(idString);
+                    }
+                    else{
+                        nodeTracer = contentDocument.getElementsByClassName(idString)
+                    }
+                    
+                  }
+        
+      
+      
+
+    }
+    
+    return nodeTracer;
+    // var id = getId(epub-cfi);
+    // var node = document.getElementById(id);
+    // return node;
 }
